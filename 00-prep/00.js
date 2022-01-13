cdae2e1f2d02de9b0e620960f2d0500ac97f5a37
dwhenson <dave.henson@gmail.com>
2022-01-13 14:09:45 +0300

    ✅ ADD: prep chapter

---
+++ 00-prep/00.js
@@ -0,0 +1,25 @@
+// 5
+// substring = String, prototype
+// create = Object, static
+// fromCharCode = String, static
+// slice = Array, prototype
+// toString = String, prototype
+
+// 6
+// lazyDog
+// operand2
+// index
+
+// 7
+// CatName
+// lazyDog
+// index
+// operand2
+
+// 8
+// BIG_NUMBER
+
+// 9
+// snake_case
+// quick_fox
+// 1stCharacter
